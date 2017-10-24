e5f8a410951e8905ae95997a75c8a3cf4b192377
Adrian <adrianjewell91@gmail.com>
2017-10-24 11:50:31 -0700

    add to databse

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -5,6 +5,18 @@ class UploadsController < ApplicationController
   end
 
   def create
+    #Upon create, I need to create a new entry and then save it.
+    debugger
+    waveform = Upload.new
+    waveform.title = params.values.first["results"]["encode"][0]["original_basename"]
+    waveform.url = params.values.first["results"]["encode"][0]["url"]
+
+    waveform.save
+    # if waveform.save
+    #   render json: {new_entry: waveform}
+    # else
+    #   render json: {error: "error!"}
+    # end
   end
 
   def index
